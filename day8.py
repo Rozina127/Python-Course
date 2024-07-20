9569c9e4cbc451f80e1145b20115a1a8b454d743
Rozina127 <rozinawalikhan@gmail.com>
2024-07-20 16:40:40 +0500

    function with parameters

--- day8.py
+++ day8.py
@@ -1,1 +1,14 @@
-print(1234)
+def greet():
+    print("Hello")
+    print("How are you?")
+    print("Where do you live?\n")
+
+# Call the function to execute it
+greet()
+
+#function which take input 
+def takename(name):
+    print(f"Your name is {name}")
+
+# Call the function with a specific input
+takename("Rozina")
