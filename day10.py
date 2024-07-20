cd4c0a5cf1bd1c45a671998411a7a49539f24dea
Rozina127 <rozinawalikhan@gmail.com>
2024-07-21 01:11:39 +0500

    function with return statement

--- day10.py
+++ day10.py
@@ -1,3 +1,4 @@
+#**************** function with return statements ***************#
 def takename(name, location):
     """Returns a formatted string with the name and location."""
     return f"Your name is {name}. You live in {location}."
@@ -5,3 +6,16 @@ def takename(name, location):
 # Call the function with a specific input and print the result
 result = takename("Rozina", "Peshawar")
 print(result)
+
+
+#************** with multiple return values **********************#
+def takename(first_name, last_name, location):
+    """Returns a formatted string with the first name, last name, and location."""
+    return f"Your name is {first_name} {last_name}. You live in {location}."
+
+# Call the function with specific inputs and print the result
+result = takename("Rozina", "Khan", "Peshawar")
+print(result)
+ 
+ 
+ 
