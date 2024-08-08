9778b02e0e95659c9adc57b100be1c3db35a8440
Rozina127 <rozinawalikhan@gmail.com>
2024-08-08 17:31:43 +0500

    day26list and project

--- day26projectnato.py
+++ day26projectnato.py
@@ -8,8 +8,20 @@ data = pandas.read_csv("day26projectnato.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def generate_phonetics():
+    word = input("Hello. Enter a word:").upper()
+    try:
+       output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+       print ("Sorry.All Alphabets is allowed to enter.")
+       generate_phonetics()
+    else:
+       print(output_list)
+
+generate_phonetics()
+
+
+
